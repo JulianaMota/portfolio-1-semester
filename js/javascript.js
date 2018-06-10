d07b46433f6e37bf00af0fb8e16a18a6a4d2e443
Juliana Mota <38047358+JulianaMota@users.noreply.github.com>
2018-06-10 19:56:29 +0200

    port

--- js/javascript.js
+++ js/javascript.js
@@ -20,6 +20,7 @@ window.onscroll = function () {
     scrollFunction()
 };
 
+
 function scrollFunction() {
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
         document.getElementById("menu").style.top = "0";
